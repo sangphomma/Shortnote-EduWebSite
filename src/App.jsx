91d0eb5450d6f384f9d1f3d1dd24ae8021a16693
sangphomma <87381359+sangphomma@users.noreply.github.com>
2023-10-02 05:44:56 +0700

    Base file structure setup finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <section>
+        <div className="container mx-auto">
+          <h1 className="font-Sarabun bg-teal-100 font-bold">
+            ผมคือประชาชน ชาวกาญจนบุรี
+          </h1>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
